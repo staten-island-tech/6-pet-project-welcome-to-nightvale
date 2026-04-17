565ab34b2779fde94965b1ebe857b2b1fae98974
screaming in agony <ameliar51@nycstudents.net>
2026-04-17 09:53:17 -0400

    ew

--- orangge.py
+++ orangge.py
@@ -15,7 +15,19 @@ class pet:
         print(f"Hunger: {self.hunger}")
         print(f"Happiness: {self.happiness}")
 
-pet_name = input("What will you name the creature?")
+pet_name = input("What will you name the creature? ")
 pett = pet(pet_name, 10, 10)
 print(f"Your pet's name is {pet_name}.")
-
+interacting = False
+while interacting == False:
+    action = input("what would you like to do with your pet? \n Your options are: \n1. feed \n2. entertain \n3. display statistics\nSelect a number: ")
+    interacting = True
+    if action == "1":
+        pett.feed()
+    if action == "2":
+        pett.entertain()
+    if action == "3":
+        pett.display()
+    else:
+        print("Action does not exist, please choose again")
+        interacting = False
